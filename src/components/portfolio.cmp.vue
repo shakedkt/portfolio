5da512918f088f57f6c3c2b417ac63e1305fa6e2
Shaked <shaked@7sjOF9KU9h.lan>
2020-05-26 12:42:33 +0300

    layout height changes

--- src/components/portfolio.cmp.vue
+++ src/components/portfolio.cmp.vue
@@ -3,107 +3,130 @@
     <h2 v-scrollAnimation>&lt; MY RECENT PROJECTS &gt;</h2>
 
     <div class="projects-container" v-scrollAnimation>
-      <a href="https://shakedinsta.herokuapp.com/#/" target="_blank">
-        <div class="image-container">
-          <img src="../images/instagram.jpg" class="image" />
-          <div class="middle">
-            <h4>Instagram clone</h4>
-            <p>My finals project, End to End Instagram clone from scratch using the latest technologies</p>
+      <div class="image-container">
+        <img src="../images/instagram.jpg" class="image" />
+        <div class="middle">
+          <h4>Instagram clone</h4>
+          <p>My finals project, End to End Instagram clone from scratch using the latest technologies</p>
 
-            <div id="instagram" class="tags">
-              <span>HTML5</span>
-              <span>SASS</span>
-              <span>ES6</span>
-              <span>VUE</span>
-              <span>VUEX</span>
-              <span>NODE.JS</span>
-              <span>MONGODB</span>
-            </div>
+          <div id="instagram" class="tags">
+            <!-- <span>HTML5</span> -->
+            <span>SASS</span>
+            <span>ES6</span>
+            <!-- <span>VUE</span> -->
+            <span>VUEX</span>
+            <span>NODE.JS</span>
+            <span>MONGODB</span>
           </div>
+
+          <a href="https://shakedinsta.herokuapp.com/#/" target="_blank" class="link-btn">
+            <button>visit website</button>
+          </a>
         </div>
-      </a>
+      </div>
 
-      <a href="https://shakedkt.github.io/appsus/#/" target="_blank">
-        <div class="image-container">
-          <img src="../images/appsus.png" class="image" />
-          <div class="middle">
-            <h4>APPSUS</h4>
-            <p>Single page application with a set of mini apps: misterEmail, missKeep, missBooks</p>
+      <div class="image-container">
+        <img src="../images/appsus.png" class="image" />
+        <div class="middle">
+          <h4>APPSUS</h4>
+          <p>Single page application with a set of mini apps: misterEmail, missKeep, missBooks</p>
 
-            <div class="tags">
-              <span>HTML5</span>
-              <span>CSS3</span>
-              <span>VUE</span>
-            </div>
+          <div class="tags">
+            <span>HTML5</span>
+            <span>CSS3</span>
+            <span>VUE</span>
           </div>
+
+          <a href="https://shakedkt.github.io/appsus/#/" target="_blank" class="link-btn">
+            <button>visit website</button>
+          </a>
         </div>
-      </a>
+      </div>
 
-      <a href="https://shakedkt.github.io/proj2-meme-generator/#social" target="_blank">
-        <div class="image-container">
-          <img src="../images/memegen.png" class="image" />
-          <div class="middle">
-            <h4>MEME GENERATOR</h4>
-            <p>Meme generator site using vanila JavaScript, HTML, CSS</p>
+      <div class="image-container">
+        <img src="../images/memegen.png" class="image" />
+        <div class="middle">
+          <h4>MEME GENERATOR</h4>
+          <p>Meme generator site using vanila JavaScript, HTML, CSS</p>
 
-            <div class="tags">
-              <span>HTML5</span>
-              <span>CSS3</span>
-              <span>ES6</span>
-            </div>
+          <div class="tags">
+            <span>HTML5</span>
+            <span>CSS3</span>
+            <span>ES6</span>
           </div>
+
+          <a
+            href="https://shakedkt.github.io/proj2-meme-generator/#social"
+            target="_blank"
+            class="link-btn"
+          >
+            <button>visit website</button>
+          </a>
         </div>
-      </a>
+      </div>
 
-      <a href="https://shakedkt.github.io/Mister-BITCoin-react-new/" target="_blank">
-        <div class="image-container">
-          <img src="../images/misterbitcoin.png" class="image" />
-          <div class="middle">
-            <h4>MISTER BITCOIN</h4>
-            <p>React project, digital bitcoin wallet, transfer bitcoin to contacts</p>
+      <div class="image-container">
+        <img src="../images/misterbitcoin.png" class="image" />
+        <div class="middle">
+          <h4>MISTER BITCOIN</h4>
+          <p>React project, digital bitcoin wallet, transfer bitcoin to contacts</p>
 
-            <div class="tags">
-              <span>HTML5</span>
-              <span>SASS</span>
-              <span>REACT</span>
-              <span>REDUX</span>
-              <span>ES6</span>
-            </div>
+          <div class="tags">
+            <span>HTML5</span>
+            <span>SASS</span>
+            <span>REACT</span>
+            <span>REDUX</span>
+            <span>ES6</span>
           </div>
+
+          <a
+            href="https://shakedkt.github.io/Mister-BITCoin-react-new/"
+            target="_blank"
+            class="link-btn"
+          >
+            <button>visit website</button>
+          </a>
         </div>
-      </a>
+      </div>
 
-      <a href="https://shakedkt.github.io/my-team/" target="_blank" class="myteam">
-        <div class="image-container">
-          <img src="../images/myteam.jpg" class="image" />
-          <div class="middle">
-            <h4>MYTEAM</h4>
-            <p>small layout project using react</p>
+      <!-- class="myteam"> -->
+      <div class="image-container">
+        <img src="../images/myteam.jpg" class="image" />
+        <div class="middle">
+          <h4>MYTEAM</h4>
+          <p>small layout project using react</p>
 
-            <div class="tags">
-              <span>HTML5</span>
-              <span>SASS</span>
-              <span>REACT</span>
-              <span>ES6</span>
-            </div>
+          <div class="tags">
+            <span>HTML5</span>
+            <span>SASS</span>
+            <span>REACT</span>
+            <span>ES6</span>
           </div>
-        </div>
-      </a>
-      <a href="https://shakedkt.github.io/mine-sweeper-2020/" target="_blank" class="minesweeper">
-        <div class="image-container">
-          <img src="../images/minesweeper.png" class="image" />
-          <div class="middle">
-            <h4>MINE SWEEPER</h4>
-            <p>Mine sweeper with few extra features including extra lives and hints, using vanila JavaScript</p>
 
-            <div class="tags">
-              <span>HTML5</span>
-              <span>CSS3</span>
-              <span>ES6</span>
-            </div>
-          </div>
+          <a href="https://shakedkt.github.io/my-team/" target="_blank" class="link-btn">
+            <button>visit website</button>
+          </a>
         </div>
-      </a>
+      </div>
+
+      <!-- class="minesweeper"> -->
+      <div class="image-container">
+        <img src="../images/minesweeper.png" class="image" />
+        <div class="middle">
+          <h4>MINE SWEEPER</h4>
+          <p>Mine sweeper with few extra features including extra lives and hints, using vanila JavaScript</p>
+
+          <div class="tags">
+            <span>HTML5</span>
+            <span>CSS3</span>
+            <span>ES6</span>
+          </div>
+
+          <a href="https://shakedkt.github.io/mine-sweeper-2020/" target="_blank" class="link-btn">
+            <button>visit website</button>
+          </a>
+        </div>
+      </div>
     </div>
   </section>
 </template>
