543eae67a8b87baf092129ea7c9cd37b6153f46e
Shaked <shaked@7sjof9ku9h.lan>
2020-05-29 17:37:09 +0300

    contactMe fix

--- src/components/contactMe.cmp.vue
+++ src/components/contactMe.cmp.vue
@@ -6,13 +6,13 @@
     </div>
 
     <div v-scrollAnimation>
-      <form target="_blank" action="https://formsubmit.co/shakedkt@walla.co.il" method="POST">
+      <form target="_blank" action="https://formsubmit.co/shakedkat@gmail.com" method="POST">
         <div class="name-and-email">
-          <input name="fullName" class="name" v-model="mailData.name" type="text" placeholder="Name" />
+          <input class="name" v-model="mailData.name" type="text" placeholder="Name" />
           <input v-model="mailData.email" type="email" name="email" placeholder="Email" />
         </div>
-        <input class="subject" v-model="mailData.subject" type="text" name="subject" placeholder="Subject" />
-        <textarea v-model="mailData.text" name="message" placeholder="Message"></textarea>
+        <input v-model="mailData.subject" type="text" name="subject" placeholder="Subject" />
+        <textarea v-model="mailData.text" name="message" placeholder="message"></textarea>
         <input type="hidden" name="_captcha" value="false" />
         <input type="hidden" name="_next" value="http://www.shakedkat.com/contact" />
 
@@ -21,6 +21,7 @@
     </div>
   </section>
 </template>
+
 
 <script>
 export default {
