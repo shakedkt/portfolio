32e2d6fbb2b07e45bb562cb4197dbde63f302e33
Shaked <shaked@7sjof9ku9h.lan>
2020-05-29 18:31:08 +0300

    cursor pointer logo

--- src/components/contactMe.cmp.vue
+++ src/components/contactMe.cmp.vue
@@ -8,17 +8,18 @@
     <div v-scrollAnimation>
       <form target="_blank" action="https://formsubmit.co/shakedkat@gmail.com" method="POST">
         <div class="name-and-email">
-          <input class="name" v-model="mailData.name" name="name" type="text" placeholder="Name" />
-          <input v-model="mailData.email" type="email" name="email" placeholder="Email" />
-        </div>
-        <input v-model="mailData.subject" type="text" name="subject" placeholder="Subject" />
-        <textarea v-model="mailData.text" name="message" placeholder="message"></textarea>
-        <input type="hidden" name="_captcha" value="false" />
-        <input type="hidden" name="_next" value="http://www.shakedkat.com/contact" />
+          <input class="name" v-model="mailData.name" name="name" type="text" placeholder="Name" >
+          <input v-model="mailData.email" type="email" name="email" placeholder="Email" >
+          </div>
+        <input class="subject" v-model="mailData.subject" type="text" name="subject" placeholder="Subject">
+        <textarea v-model="mailData.text" name="message" placeholder="Message" required></textarea>
+        <input type="hidden" name="_captcha" value="false">
+        <input type="hidden" name="_next" value="http://www.shakedkat.com/contact">
 
         <button @click="submitTheForm" type="submit">Send</button>
       </form>
     </div>
+
   </section>
 </template>
 
@@ -48,5 +49,4 @@ export default {
   opacity: 1;
   transform: scale(1);
 }
-
 </style>
